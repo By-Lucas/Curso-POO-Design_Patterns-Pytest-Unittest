299fc1808268e648a008d150c160930a8fca04e6
By-Lucas <lukasmulekezika2@gmail.com>
2022-11-24 22:27:15 -0300

    Pytest examples

---
+++ pytest/test_pytest_1.py
@@ -0,0 +1,12 @@
+
+def mult(x, y):
+    return x * y
+
+def somar(x , y):
+    return x + y
+
+def test_somar():
+    assert somar(10, 20) == 30
+
+def test_ult():
+    assert mult(2, 3) == 6
---
+++ pytest/test_pytest_2.py
@@ -0,0 +1,7 @@
+class TestExample:
+    def test_one(self):
+        linguage = 'python'
+        assert 'py' in linguage
+    
+    def test_two(self):
+        assert 10 == 10
